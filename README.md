5ccef098d9c2d219cb53930743a4941f8fe1c214
Jan Strnádek <jan.strnadek@gmail.com>
2022-06-15 14:26:46 +0200

    Init version

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# aws-s3-cleaner
-Github Actions CI task, to clean up your s3 bucket
+# AWS S3 Cleanup task
+
+Runs a cleanup process for S3 bucket.
+
+Usage
+``` yaml      
+- name: Get date 30 days ago for S3 cleanup 
+  id: date
+  run: echo "::set-output name=date::$(date -v-30d  +'%Y-%m-%d')"
+
+- name: Run cleanup
+  uses: trustfractal/aws-s3-cleanup@v1.0
+  with:
+    bucket: my-bucket
+    include: |
+      src/**/*.js
+      locale/**/*.po
+    exclude: |
+      vendor/*
+    older_than: ${{steps.date.outputs.date}}
+```
+
+See [action.yml](action.yml) file for the full documentation of this action's inputs and outputs.
+
+For matching, we are using [wildcard-match](https://www.npmjs.com/package/wildcard-match#usage) npm package.
+
+## License Summary
+
+This code is made available under the MIT license.
---
+++ index.js
@@ -0,0 +1,80 @@
+const core = require("@actions/core");
+const AWS = require("aws-sdk");
+const wmatch = require("wildcard-match");
+
+const s3 = new AWS.S3();
+
+const cleanArray = (value) => (value || [])
+  .map(x => x.trim())
+  .filter(x => x.length > 0)
+  .map(wmatch);
+
+const main = async () => {
+  const bucket = core.getInput("bucket", { required: true });
+  const dryRun = core.getBooleanInput("dry_run");
+  
+  const include = cleanArray(core.getMultilineInput("include"));
+  const exclude = cleanArray(core.getMultilineInput("exclude"));
+  
+  let olderThan = core.getInput("older_than");
+  if (olderThan) {
+    olderThan = new Date(olderThan);
+
+    if (olderThan.toString() === "Invalid Date") {
+      core.setFailed("Invalid date provided fro older_than attribute.");
+      throw new Error("Invalid date for older_than input.");
+    }
+  }
+
+  core.debug("Iterate over files and test include, exclude filters and dates...");
+  let isTruncated = true;
+  let lastMarker = undefined;
+
+  try {
+    while (isTruncated) {
+      const { Contents, ...rest } = await s3.listObjects({
+        Bucket: bucket,
+        Marker: lastMarker,
+      }).promise();
+
+      for (const item of Contents) {
+        lastMarker = item.Key;
+
+        // Is there any excluded filter for this key?
+        const excluded = exclude.find(x => x(item.Key));
+        if (excluded) {
+          continue;
+        }
+
+        // Is include filter present and exists at least one match?
+        if (included.length > 0 && !include.find(x => x(item.Key))) {
+          continue;
+        }
+
+        // Older than is present, but the file is younger
+        if (olderDate && item.LastModified.getTime() > olderDate.getTime()) {
+          continue;
+        }
+
+        if (dryRun) {
+          console.log(`-> Item ${item.Key} will be removed`);
+        } else {
+          await s3.deleteObject({
+            Bucket: bucket,
+            Key: item.Key,
+          }).promise();
+        }
+      }
+
+      isTruncated = rest.IsTruncated;
+
+      if (rest.NextMarker) {
+        lastMarker = rest.NextMarker;
+      }
+    }
+  } catch (e) {
+    core.setFailed(e);
+  }
+};
+
+main();
